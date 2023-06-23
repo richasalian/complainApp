d6c4e0842356612cab1404625094288b704e5118
Tek N. Chaudhary <tekchau21@gmail.com>
2023-06-23 19:20:57 +0530

    Sign up and login changes

--- app/src/main/java/com/example/complaintapp/waterpage.java
+++ app/src/main/java/com/example/complaintapp/waterpage.java
@@ -18,14 +18,12 @@ DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        db=FirebaseDatabase.getInstance().getReference("Complaints");
         setContentView(R.layout.activity_waterpage);
         comp1 = (EditText) findViewById(R.id.editTextTextMultiLine2);
         loc1 = (EditText) findViewById(R.id.editTextTextPostalAddress2);
         submit1 = (Button) findViewById(R.id.button3);
         String wcomp = getIntent().getStringExtra("complaint1");
         String wloc = getIntent().getStringExtra("Location1");
-        db=FirebaseDatabase.getInstance().getReference("water");
         submit1.setOnClickListener(view -> {
             insertdata();
             Intent i = new Intent(waterpage.this, mailpage.class);
@@ -35,6 +33,7 @@ DatabaseReference db;
         });
     }
             private void insertdata() {
+                db=FirebaseDatabase.getInstance().getReference("Water Complaints");
                 String waterq = comp1.getText().toString();
                 String waterl = loc1.getText().toString();
                 ReadWriteComplaints write=new ReadWriteComplaints(waterl,waterq);
